69a002c65cc6630fc73174e62d095143b0248c6c
Diego <davc93@gmail.com>
2022-10-12 15:54:10 -0300

    clase 20

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { XCircleIcon } from '@heroicons/react/solid';
+import { PlusIcon } from "@heroicons/react/20/solid"
 
 export default function Modal({ open, setOpen, children }) {
   const cancelButtonRef = useRef(null);
@@ -25,7 +25,8 @@ export default function Modal({ open, setOpen, children }) {
           >
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <XCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-400 cursor-pointer" aria-hidden="true" onClick={() => setOpen(false)} ref={cancelButtonRef} />
+              <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              <span>Add Product</span>
               </div>
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">{children}</div>
